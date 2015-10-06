69c4e9ec2b5e62e4a87d306c575fcdc5ec480d8b
patelharsh94 <patelharsh94@gmail.com>
2015-10-06 18:51:36 -0500

    added modifiers

--- player.java
+++ player.java
@@ -7,40 +7,59 @@ public class player
   private String playerToken; //Differentiates the player from
   private int boardLocation; //Location where the player resides on the
 
-//THIS IS CHRISTIAN
+  player()
+  // POST: Sets a new board location for the specific player.
+  {
+    this(1500, 0, 0, 0,0," ", 0);
+  }
+  player(double money, int railroads, int utility, int spaceGo, String token, int location)
+  //PRE : 0 < location < 41; location is in block.
+  {
+    this.money = money;
+    this.numRailroad = railroads;
+    this.numUtility = utility;
+    this.spaceFromGo = spaceGo;
+    this.playerToken = token;
+    this.boardLocation = location;
+  }
 
-player()
-// POST: Sets a new board location for the specific player.
-{
-  this(1500, 0, 0, 0,0," ", 0);
-}
-player(double money, int railroads, int utility, int spaceGo, String token, int location)
-//PRE : 0 < location < 41; location is in block.
-{
-  this.money = money;
-  this.numRailroad = railroads;
-  this.numUtility = utility;
-  this.spaceFromGo = spaceGo;
-  this.playerToken = token;
-  this.boardLocation = location;
-}
+  public int getMoney()
+  // POST: FCTVAL == 	The amount of money a player has.
+  {
+    return this.money
+  }
 
-public int getMoney()
-// POST: FCTVAL == 	The amount of money a player has.
-{
-  return this.money
-}
+  public int getNumberRailroad()
+  // POST: FCTVAL == The number of railroads a player owns.
+  {
+    return this.numRailroad;
+  }
 
-public int getNumberRailroad()
-// POST: FCTVAL == The number of railroads a player owns.
-{
-  return this.numRailroad;
-}
+  public int getNumberUtilities()
+  // POST: FCTVAL == The number of utilities a player has.
+  {
+    return this.numUtility;
+  }
 
-public int getNumberUtilities()
-// POST: FCTVAL == The number of utilities a player has.
-{
-  return this.numUtility;
-}
+  addMoney (double money)
+  // PRE: money must be initialized, money is in dollars.
+  // POST: adds money to member variable money. If money is a
+  //       negative value, then we subtract from the member variable money.
+  {
+    this.money += money;
+  }
+
+  addNumRailroad ()
+	// POST: adds one to the class member numRailroad.
+  {
+    this.numRailroad++;
+  }
+
+  addNumUtilities ()
+	// POST: adds one to the class member numUtilities.
+  {
+    this.numUtilities++;
+  }
+
 
 }//End Player Class
