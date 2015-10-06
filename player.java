915cd65f380f5e4d915796a4c3434f0fb371d71a
Adolfo Moreno <asmoren2@uic.edu>
2015-10-06 18:47:57 -0500

    setter

--- player.java
+++ player.java
@@ -3,9 +3,10 @@ public class player
   private double money; //The amount of money that the player has
   private int numRailroad; //Number of railroads that the player owns
   private int numUtility; //Number of utilities that the player owns
-  private int spaceFromGo; //Number of spaces the player is from Go tile
+  private int spaceFromGo; //spaceFromGo keeps track of how far a player is from go
   private String playerToken; //Differentiates the player from
   private int boardLocation; //Location where the player resides on the
+  private int turnNum; //turnNum holds when a player’s turn is within the turn rotation
 
 //THIS IS CHRISTIAN
 
@@ -25,13 +26,20 @@ player(double money, int railroads, int utility, int spaceGo, String token, int 
   this.boardLocation = location;
 }
 
+public void setBoardLocation(int location)
+// PRE: 0 < location < 41; location is in block.
+//POST: Sets a new board location for the specific player.
+{
+  this.boardLocation = location;
+}
+
 public int getMoney()
 // POST: FCTVAL == 	The amount of money a player has.
 {
   return this.money
 }
 
-public int getNumberRailroad()
+public String getNumberRailroad()
 // POST: FCTVAL == The number of railroads a player owns.
 {
   return this.numRailroad;
@@ -42,5 +50,6 @@ public int getNumberUtilities()
 {
   return this.numUtility;
 }
+
 
 }//End Player Class
