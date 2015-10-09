85ea1deb6a13a729bb46d8c5369ce5967c6cce3f
Adolfo Moreno <asmoren2@uic.edu>
2015-10-09 16:35:44 -0500

    railroad class update

--- boardLocation.java
+++ boardLocation.java
@@ -1,44 +1,44 @@
 
 public abstract class boardLocation {
-    
+
     protected String nameOfLocation;    // The name of the location.
     protected int spacesFromGo;          // The distance from "go".
-    
+
     public void setSpacesFromGo (int numSpaces)
     // PRE: numSpaces must be initialized
     // POST: sets the class member spaceFromGo to numSpaces.
     {
         this.spacesFromGo = numSpaces;
     }
-    
+
     public void setName (String name)
     // PRE: name must be initialized
-    // POST: sets the cass memeber nameOfLocation to name.
+    // POST: sets the class memeber nameOfLocation to name.
     {
         this.nameOfLocation = name;
     }
-    
+
     public int getSpacesFromGo()
     // POST: FCTVAL == The class member spacesFromGo, it represents
     //                 the distance away from "go"
     {
         return this.spacesFromGo;
     }
-    
+
     public String getName()
     // POST: FCTVAL == The class member nameOfLocation, it represents
     //                 the name of the location.
     {
         return this.nameOfLocation;
     }
-    
+
     public String getPossibleActions(Player player)
     // POST: returns all the possible action a player can do on a certain
     //       board location.
     {
         return "";
     }
-    
+
     public String toString()
     // POST:  returns a string representing the object of the board location
     //        class.
@@ -46,5 +46,5 @@ public abstract class boardLocation {
         return "The name of this location is : " + nameOfLocation
               +"The location is " + spacesFromGo +" away from go.";
     }
-    
+
 }
--- railroad.java
+++ railroad.java
@@ -1,4 +1,49 @@
-public class railroad
+public class railroad extends boardLocation
 {
+//Railroad class
+//Short Line Railroad = cost 200, Tile 35
+//Reading Railroad = cost 200, Tile 5
+//Pennsylvania Railroad = cost 200, Tile 15
+//B&O Railroad = cost 200, Tile 25
 
+   //Data Dictionary
+   private static final rCost;
+
+   public void railroad()
+   //POST:
+   {
+
+   }//End Default Constructor
+
+//Method 1. Pass as String
+//Method 2. declare a number for each radilRoad type and pass in
+//          a number having already given it a signification.
+   public void railroad(String rType)
+   //POST: Create a railroad object with a cost of 200 and a tile number
+   {
+      super();
+   }
+
+   public double calcRent(int n)
+   //PRE: n is the number of radilRoads a player owns. It is assumed that the
+   //    player can own no less than 0 and no more than 4.
+   {
+      double rentPayable = 0.0;
+      if(int n == 1)
+      {
+         rentPayable = 25.0;
+      }
+      else if( n == 2)
+      {
+         rentPayable = 50.0;
+      }
+      else if(n == 3)
+      {
+         rentPayable = 100.0;
+      }
+      else if(n == 4)
+      {
+         rentPayable = 200.0;
+      }
+   }//End calcRent
 }
