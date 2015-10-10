26b9c99f237ba23e1380ba791806b6e9ccc5783b
Cvalladares4837 <Cvalla4@uic.edu>
2015-10-10 11:22:38 -0500

    cornerSquare and its constructors, modifiers, and getters
    This is a concrete class, and has no sub classes inside of it as of now.
    It is based on booleans in order to determine whether this cornerSquare
    is a Jail, a Go, or a FreeSpace.
    
    The only member variables are booleans: isGo, isJail, isFreeSpace
    
    These booleans are private,  there are setters to turn a corner square
    into a single one of these options at any given time...
    
    Further,  we have getters to verify the status of each of these booleans
    individually.

--- cornerSquare.java
+++ cornerSquare.java
@@ -1,5 +1,84 @@
-
-public class cornerSquare
+//Programmer:  Christian M. Valladares
+//Assignment:  Monopoly
+//Date:        October 8, 2015
+//Description: This class represents the four different corners of a monopoly
+//             game:  Go, Jail/ Just Visiting,  Free Parking, and Go to Jail.
+//             For this game, we do not implement Jail, Free Parking, and go to
+//             jail.   For this reason, as of now,  cornerSquare will be concrete
+//             class, only used to determine whether it is a jail corner or not
+//
+public class cornerSquare extends boardLocation
 {
+   private boolean isJail;       // isJail determines whether this corner Square
+                                 //    is a jail location.
 
+   private boolean isGo;         // isGo determines whether this corner Square
+                                 //    is the Go block.
+
+   private boolean isFreeSpace;  // isFreeSpace determines whether this corner is
+                                 //    is just a free space.
+
+   //CONSTRUCTOR
+   //........................................................................
+   public void cornerSquare()
+   // POST: cornerSquare() creates an instance of cornerSquare, with boolean
+   //       values isJail, isGo, isFreeSpace = false;
+   {
+      super();             //construct boardLocation superClass
+      isJail = false;
+      isGo = false;
+      isFreeSpace = false;
+   }
+
+   //MODIFIER
+   //........................................................................
+   public void setToJail()
+   // PRE: cornerSquare instance has been constructed, and isJail = false
+   // POST: cornerSquare's isJail = true.  isGo, isFreeSpace = false.
+   {
+      isJail = true;
+      isGo   = false;
+      isFreeSpace = false;
+   }
+
+   public void setToGo()
+   // PRE: cornerSquare instance has been constructed, and isGo = false
+   // POST: cornerSquare's isGo = true.  isJail, isFreeSpace = false
+   {
+      isGo = true;
+      isJail = false;
+      isFreeSpace = false;
+   }
+
+   public void setToFreeSpace()
+   // PRE: cornerSquare instance has been constructed, and isFreeSpace = false
+   // POST: cornerSquare's isFreeSpace = true.  isJail, isGo = false
+   {
+      isFreeSpace = true;
+      isJail = false;
+      isGo = false;
+   }
+
+   //GETTER
+   //...........................................................................
+   public boolean getJailStatus()
+   // POST: Determines whether this cornerSquare instance is a Jail square
+   //       FCTVAL = isJail
+   {
+      return isJail;
+   }
+
+   public boolean getGoStatus()
+   // POST: Determines whether this cornerSquare instance is a Go square
+   //
+   {
+      return isGo;
+   }
+
+   public boolean getFreeSpaceStatus()
+   // POST: Determines whether this cornerSquare instance is a Free Space square
+   //       FCTVAL = isFreeSpace
+   {
+      return isFreeSpace;
+   }
 }
