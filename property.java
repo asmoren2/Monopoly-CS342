6e8a85f95752dd427dbf4de3ad25ac5fb5ef8e23
patelharsh94 <patelharsh94@gmail.com>
2015-10-09 23:18:29 -0500

    property class first draft
    This is the first draft of the property class, it does most of what we intend for the property class.

--- property.java
+++ property.java
@@ -1,4 +1,46 @@
-public class property
+
+public abstract class property extends boardLocation
 {
-  // TESTING!!!!
+    protected player owner;             // The owner of the property.
+    protected double purchaseCost;      // The cost of purchasing.
+    protected double [] rentStructure;  // The rent structure of this property.
+    protected boolean isOwned;          // is the district owned or not
+    
+    public void setRent(double rent, int option)
+    // PRE: rent and option are initialized.
+    //      rent >= 0
+    //      option >= 0
+    // POST: Sets the class member rentStructure[option] to the rent.
+    {
+        this.rentStructure[option] = rent;
+    }
+    
+    public void buy(player owner)
+    // PRE: owner must be initialized.
+    // POST: sets the class member owner to owner.
+    //       sets the class member isOwned to true.
+    {
+        this.owner = owner;
+        this.isOwned = true;
+    }
+    
+    public boolean isOwned()
+    // POST: FCTVAL == isOwned.  is the property owned or not?
+    {
+        return this.isOwned;
+    }
+    
+    public double getPurcaseCost()
+    // FCTVAL == purchaseCost.  The purchase cost of the current
+    //           property.
+    {
+        return this.purchaseCost;
+    }
+    
+    public double getRent(int numHouses)
+    // FCTVAL == The rent of the property, with numHouses houses on it.
+    {
+        return this.rentStructure[numHouses];
+    }
+    
 }
