0f21f0bab1719c4f9d010e59e541a1470d243776
patelharsh94 <patelharsh94@gmail.com>
2015-10-13 09:37:45 -0500

    Read Full Description.
    Intialized and added a datatype to rCost, returned rentPayable to calcRent

--- railroad.java
+++ railroad.java
@@ -7,7 +7,7 @@ public class railroad extends property
 //B&O Railroad = cost 200, Tile 25
 
    //Data Dictionary
-   private static final rCost;//represents the total cost to own 1 railroad
+   private static final double rCost = 0;//represents the total cost to own 1 railroad
 
    public void railroad()
    //POST:
@@ -29,7 +29,7 @@ public class railroad extends property
    //    player can own no less than 0 and no more than 4.
    {
       double rentPayable = 0.0;
-      if(int n == 1)
+      if( n == 1)
       {
          rentPayable = 25.0;
       }
@@ -45,6 +45,7 @@ public class railroad extends property
       {
          rentPayable = 200.0;
       }
+      return rentPayable;
    }//End calcRent
    public double getRailCost()
    //POST: FCTVAL == the class member rCost, it represents the cost to own
