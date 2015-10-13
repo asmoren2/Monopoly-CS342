f8895693b594ec1fd2a24109bccbdc469f997b77
patelharsh94 <patelharsh94@gmail.com>
2015-10-13 13:20:19 -0500

    Added a default and initializer constructor.

--- boardLocation.java
+++ boardLocation.java
@@ -1,44 +1,61 @@
 
-public abstract class boardLocation {
-
-    protected String nameOfLocation;    // The name of the location.
-    protected int spacesFromGo;          // The distance from "go".
-
+public abstract class boardLocation
+{
+    
+    protected String nameOfLocation;     // The name of the location.
+    protected String [] possibleActions; // All the possible actions 
+                                         // a player can have.
+    protected int spacesFromGo;          // The distance from "go". 
+    
+    boardLocation()
+    {
+        nameOfLocation = "";
+        spacesFromGo = 0;
+        possibleActions = new String[20];
+    }
+    
+    boardLocation(String nameOfLocation, int spacesFromGo)
+    {
+        this.nameOfLocation = nameOfLocation;
+        this.spacesFromGo = spacesFromGo;
+    }
+    
     public void setSpacesFromGo (int numSpaces)
     // PRE: numSpaces must be initialized
     // POST: sets the class member spaceFromGo to numSpaces.
     {
         this.spacesFromGo = numSpaces;
     }
-
+    
     public void setName (String name)
     // PRE: name must be initialized
-    // POST: sets the class memeber nameOfLocation to name.
+    // POST: sets the class member nameOfLocation to name.
     {
         this.nameOfLocation = name;
     }
-
+    
     public int getSpacesFromGo()
     // POST: FCTVAL == The class member spacesFromGo, it represents
     //                 the distance away from "go"
     {
         return this.spacesFromGo;
     }
-
+    
     public String getName()
     // POST: FCTVAL == The class member nameOfLocation, it represents
     //                 the name of the location.
     {
         return this.nameOfLocation;
     }
-
-    public String getPossibleActions(Player player)
-    // POST: returns all the possible action a player can do on a certain
-    //       board location.
+    // TODO:: This is a little confusing , ask Prof.Hogan.
+    public String[] getPossibleActions(player player)
+    // POST: FCTVAL == The class member possibleActions, it represents
+    //                 all the possible actions a player can do at the 
+    //                 current board location.
     {
-        return "";
+        return possibleActions;
     }
-
+    
     public String toString()
     // POST:  returns a string representing the object of the board location
     //        class.
@@ -46,5 +63,5 @@ public abstract class boardLocation {
         return "The name of this location is : " + nameOfLocation
               +"The location is " + spacesFromGo +" away from go.";
     }
-
+    
 }
