59ed625f4d7850c90c527ca2b86938e3733aa920
patelharsh94 <patelharsh94@gmail.com>
2015-10-15 00:58:05 -0500

    updated

--- utility.java
+++ utility.java
@@ -1,3 +1,8 @@
+// Programmer:  Adolfo Moreno
+// Assignment:  Monopoly
+// Date:        October, 14 2015
+// Description: This class represents a utility on the board location. 
+
 
 public class utility extends property
 {
@@ -6,22 +11,25 @@ public class utility extends property
    //Water Works, Cost : 150, Tile: 28
 
    public utility()
-   //POST:
+   // POST: a default utility is created with the purchase
+   //      cost of 150.
    {
-	   this.purchaseCost = 150;
-       
+       super();
+       this.purchaseCost = 150;
    }//Default Constructor
 
-   public utility(String name, int spacesFromGo)
+   public utility(String nameOfLocation, int spacesFromGo)
+   // POST: a default utility is created with the name 
+   //       nameOfLocation and it is spacesFromGo away from
+   //       the go block.  It cost purchaseCost.
    {
-      this.nameOfLocation = name;
-      this.spacesFromGo = spacesFromGo;
+      super(nameOfLocation,spacesFromGo);
       this.purchaseCost = 150.0;
    }//input Constructor
 
    public double caltUtilRent(int n, int diceNumber)
    {
-	  double rentCost = 0;
+      double rentCost = 0;
       if(n == 1)
       {
          rentCost = (4*diceNumber);
