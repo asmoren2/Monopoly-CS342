b9237a47491320a49e4ddb94dcaef74bb82f5c35
Adolfo Moreno <asmoren2@uic.edu>
2015-10-18 16:48:05 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -13,15 +13,34 @@ public class GUI extends JApplet implements ActionListener
                                         // the user pressed next turn.
     private int turnCounter;            // Counter for turn.
     private JButton nextTurn;
-
+    private JButton buyLocation;
+    
     @Override
     public void init()
     {
         JPanel south = new JPanel();
+        JPanel north = new JPanel();
+        JPanel east = new JPanel();
+        JPanel west = new JPanel();
+        JPanel center = new JPanel();
+        JPanel northCenter = new JPanel();
+        JPanel southCenter = new JPanel();
         BorderLayout layout;
         layout = new BorderLayout();
         turnCounter = 0;
         isNextTurn = true;
+        
+        //Test different colors
+        south.setBackground(Color.BLUE);
+        north.setBackground(Color.RED);
+        east.setBackground(Color.GRAY);
+        west.setBackground(Color.GREEN);
+        northCenter.setBackground(Color.BLACK);
+        southCenter.setBackground(Color.YELLOW);
+        center.setLayout(new GridLayout(2,1));
+        center.setBackground(Color.CYAN);
+
+        
         
         setLayout(layout);
         setSize(600,600);
@@ -36,7 +55,15 @@ public class GUI extends JApplet implements ActionListener
         nextTurn = new JButton("Next Turn");
         nextTurn.addActionListener(this);
         south.add(nextTurn);
+        center.add(northCenter);
+        center.add(southCenter);
         add(BorderLayout.SOUTH, south);
+        add(BorderLayout.NORTH, north);
+        add(BorderLayout.EAST, east);
+        add(BorderLayout.WEST, west);
+        add(BorderLayout.CENTER, center);
+  
+
         
     }
     
