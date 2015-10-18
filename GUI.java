317f578c790722b479e580e1b9483b220d8e2ddb
Cvalladares4837 <Cvalla4@uic.edu>
2015-10-18 18:30:35 -0500

    Fixes on Gui

--- GUI.java
+++ GUI.java
@@ -14,34 +14,55 @@ public class GUI extends JApplet implements ActionListener
     private int turnCounter;            // Counter for turn.
     private JButton nextTurn;
     private JButton buyLocation;
-    
+    JButton playerProp [];              // playerInfo is an array of buttons
+                                        //   to fetch  properties for players 1-4
+    JLabel  playerStatus[];             // A label to determine basic player infor
+                                        //   like current position, and funds
+    int tmpPlayerLocation;              // An Integer to hold where a player's location
+                                        //   at a given moment.
+    double tmpPlayerFunds;                 // An Integer to hold a player's fund on a
+                                        //   given instant
+
+    JPanel south;
+    JPanel north;
+    JPanel east ;
+    JPanel west;
+    JPanel center;
+    JPanel northCenter;
+    JPanel southCenter;
+    JPanel east1L;
+    JPanel east2L;
+    JPanel east3L;
+    JPanel east4L;
+    JPanel east5L;
+    JPanel east1R;
+    JPanel east2R;
+    JPanel east3R;
+    JPanel east4R;
+    JPanel east5R;
+
     @Override
     public void init()
     {
-        JPanel south = new JPanel();
-        JPanel north = new JPanel();
-        JPanel east = new JPanel();
-        JPanel west = new JPanel();
-        JPanel center = new JPanel();
-        JPanel northCenter = new JPanel();
-        JPanel southCenter = new JPanel();
-        JPanel east1L = new JPanel();
-        JPanel east2L = new JPanel();
-        JPanel east3L = new JPanel();
-        JPanel east4L = new JPanel();
-        JPanel east5L = new JPanel();
-        JPanel east1R = new JPanel();
-        JPanel east2R = new JPanel();
-        JPanel east3R = new JPanel();
-        JPanel east4R = new JPanel();
-        JPanel east5R = new JPanel();
+        //Set up all panels
+        south = new JPanel();
+        north = new JPanel();
+        east = new JPanel();
+        west = new JPanel();
+        center = new JPanel();
+        northCenter = new JPanel();
+        southCenter = new JPanel();
+        east1L = new JPanel();
+        east2L = new JPanel();
+        east3L = new JPanel();
+        east4L = new JPanel();
+        east5L = new JPanel();
+        east1R = new JPanel();
+        east2R = new JPanel();
+        east3R = new JPanel();
+        east4R = new JPanel();
+        east5R = new JPanel();
 
-        
-        BorderLayout layout;
-        layout = new BorderLayout();
-        turnCounter = 0;
-        isNextTurn = true;
-        
         //Test different colors
         south.setBackground(Color.BLUE);
         north.setBackground(Color.RED);
@@ -63,23 +84,50 @@ public class GUI extends JApplet implements ActionListener
         east4R.setBackground(Color.GRAY);
         east5R.setBackground(Color.WHITE);
 
+        //Initialize the Get Player Property section and Player values section.
+        playerProp = new JButton [5];       //Initialize the array itself
+        playerStatus = new JLabel [5];
 
+        BorderLayout layout;
+        layout = new BorderLayout();
+        turnCounter = 0;
+        isNextTurn = true;
 
-
-
-
-        
-        
         setLayout(layout);
-        setSize(600,600);
-        playerList = new player[3];        
+      //  setSize(600,600);
+        playerList = new player[4];
         playerList[0] = new player(1500, 0, "Harsh");
         playerList[1] = new player(1500, 0, "Adolfo");
         playerList[2] = new player(1500, 0, "Christian");
-        
+        playerList[3] = new player(1500, 0, "Cortellano");
+
+        //Populate the players with their information.
+        for(int i = 0; i < 4; i++)
+        {
+            tmpPlayerLocation = playerList[i].getBoardLocation();
+            tmpPlayerFunds = playerList[i].getMoney();
+            //Initialize the actual buttons and labels with the expected
+            //   information
+            playerProp[i] = new JButton("Player " + i + " Properties.");
+            playerStatus[i] = new JLabel("Location: " + tmpPlayerLocation +
+                                         "\nFunds: "  + tmpPlayerFunds);
+        }
+
+        east1L.add(playerProp[0]);
+        east2L.add(playerStatus[0]);
+        east3L.add(playerProp[1]);
+        east4L.add(playerStatus[1]);
+        east5L.add(playerProp[2]);
+        east1R.add(playerStatus[2]);
+        east2R.add(playerProp[3]);
+        east3R.add(playerStatus[3]);
+//        east4R.add(playerProp[4]);
+//        east5R.add(playerStatus[4]);
+
+
         theBank = new player (9999,0, "Bank");
         theGame = new Monopoly(playerList);
-        
+
         nextTurn = new JButton("Next Turn");
         nextTurn.addActionListener(this);
         south.add(nextTurn);
@@ -96,17 +144,16 @@ public class GUI extends JApplet implements ActionListener
         east.add(east4R);
         east.add(east5R);
 
+        add(layout.SOUTH, south);
+        add(layout.NORTH, north);
+        add(layout.EAST, east);
+        add(layout.WEST, west);
+        add(layout.CENTER, center);
 
-        add(BorderLayout.SOUTH, south);
-        add(BorderLayout.NORTH, north);
-        add(BorderLayout.EAST, east);
-        add(BorderLayout.WEST, west);
-        add(BorderLayout.CENTER, center);
-  
 
-        
+
     }
-    
+
     @Override
     public void paint(Graphics g)
     {
@@ -114,25 +161,25 @@ public class GUI extends JApplet implements ActionListener
         {
             super.paint(g);
             isNextTurn = false;         // Reset
-            g.drawString("Player #: " + turnCounter+ "\n", 250, 250);
+            g.drawString("Player : " + turnCounter+ "\n", 250, 250);
             if(turnCounter >= playerList.length)
             {
                 turnCounter  = 0;
             }
         }
-        
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
         if(e.getSource() == nextTurn)
         {
             turnCounter++;
             isNextTurn = true;
         }
+
         repaint();
     }
-    
-    
+
+
 }
