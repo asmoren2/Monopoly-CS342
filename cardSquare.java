e2d9f9e70b8481b86756eafe00563ab205d5bffa
Cvalladares4837 <Cvalla4@uic.edu>
2015-10-22 09:33:44 -0500

    cardSquare Updated

--- cardSquare.java
+++ cardSquare.java
@@ -1,18 +1,18 @@
-//Programmer:  Christian M. Valladares
+//Programmers:  Christian M. Valladares, Harsh Patel, Adolfo Moreno
 //Assignment:  Monopoly
 //Date:        October 8, 2015
 //Description: This class represents the chance and community chest locations.
 //             The class will simply generate an amount to be deducted or
-//             accrued in a player's account.  within the player defined bounds
+//             accrued in a player's account; Within the player defined bounds.
 //
 public class cardSquare extends boardLocation
 {
    private double bound;        // bound determines the bounds to generate the random
-                                //   values
+                                // values
    private double valueReturned;// valueReturned holds the last value returned for
-                                //   the cardSquare
+                                // the cardSquare
    private boolean isPositive;  // isPositive determines whether value returned will
-                                //   be added or subtracted from the user account
+                                // be added or subtracted from the user account
 
    public cardSquare()
    // POST: cardSquare creates an instance of cardSquare, where bound = 200,
@@ -27,7 +27,7 @@ public class cardSquare extends boardLocation
    public cardSquare(String nameOfLocation, int spacesFromGo, double bound)
    // POST: cardSquare creates an instance of cardSquare, where its super class is
    //       constructed.  Further, bound = bound,  and isPositive = true by default
-   //       valueReturned = 0;
+   //       valueReturned = 0
    {
        super(nameOfLocation,spacesFromGo);
        this.bound = bound;
@@ -39,24 +39,24 @@ public class cardSquare extends boardLocation
    // POST: FCTVAL = -bound <= x <= bound
    {
        isPositive = (Math.random() > .5);  // Update the isPositive value to a randomly
-                                           //   generated boolean value, considering that
-                                           //   Math.random generates decimals 0 < x < 1
+                                           // generated boolean value, considering that
+                                           // Math.random generates decimals 0 < x < 1
 
 
 
-       if (isPositive == true)                  // if isPositive is true, return
-          return (Math.random() * bound);       //  a positive random value
+       if (isPositive == true)                    // if isPositive is true, return
+       {
+           return (Math.random() * bound);        // a positive random value
+       }
 
-       else                                     // if isPositive is negative, return a
-         return (Math.random() * bound) * (-1); //   negative random value
-
+       else                                       // if isPositive is negative, return a
+       {
+           return (Math.random() * bound) * (-1); // negative random value
+       }
    }
-
-
 
    public String toString()
     // POST:  returns a string representing the object of the cardSquare class
-    //
     {
         return "The name of this location is : " + nameOfLocation
               +"\nThe location is " + spacesFromGo +" away from go."
@@ -65,41 +65,37 @@ public class cardSquare extends boardLocation
 
     @Override
     public String [] getPossibleActions(player player)
+    // PRE: player must be initialized.
+    // POST: FCTVAL == possibleActions, an array of Strings containing all
+    //                 the possible actions a player can perform at a cardSquare
+    //                 is returned.  
+    //                 An array of booleans that represents the same thing is also 
+    //                 altered in order to be used in the GUI.
     {
 
        // give the money to the player.
        player.addMoney(getChanceValue());
 
-       // end game
-       actionStatus[4] = true;
+       actionStatus[4] = true;                                 // end game
        possibleActions[4] = PACTIONS[4];
-       // Do nothing
-       actionStatus[0] = true;
+
+       actionStatus[0] = true;                                 // Do nothing
        possibleActions[0] = PACTIONS[0];
-       if(player.getMoney() > 0
-          && player.canImprove(player.getImprovingLots()))
-       // if the player has money and can improve lots
+       
+       if(player.getMoney() > 0                                // if the player has 
+          && player.canImprove(player.getImprovingLots()))     // money and can improve 
+                                                               // lots
        {
            actionStatus[3] = true;
            possibleActions[3] = PACTIONS[3];
        }
-       else if(player.getMoney() < 0
-               && player.hasSellableProperty())
-       // player has no money and can sell lots
+       else if(player.getMoney() < 0                           // player has no money 
+               && player.hasSellableProperty())                // and can sell lots
        {
-           // allow to sell
-           actionStatus[2] = true;
+           actionStatus[2] = true;                             // allow to sell
            possibleActions[2] = PACTIONS[2];
        }
 
        return possibleActions;
     }
-
-    @Override
-    public boolean performAction(player thePlayer, player theBank, char choice) {
-        // TODO Auto-generated method stub
-        return true;
-    }
-
-
 }
