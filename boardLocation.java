f39c1f9407f2183ccbae45d8603d28f60002f645
Cvalladares4837 <Cvalla4@uic.edu>
2015-10-22 09:31:37 -0500

    boardLocation updated

--- boardLocation.java
+++ boardLocation.java
@@ -1,20 +1,22 @@
-// Programmer:  Harsh Patel
+// Programmers:  Harsh Patel, Christian Valaderas and Adolfo Moreno
 // Assignment:  Monopoly
 // Date:        October, 14 2015
 // Description: This class acts as a super class to all the other
-//              monopoly classes, it represents a board location
+//              monopoly classes, it represents a board location such that
 //              through the use of inheritance, we can change this
 //              class to represent more specific locations of the board.
 public abstract class boardLocation
 {
     
-    protected String nameOfLocation;           // The name of the location.
-    protected static final String [] PACTIONS = {"Do Nothing", "Buy", 
-                                                 "Sell", "Improve Property", 
-                                                 "End Game"};    
-
+    protected String nameOfLocation;                // The name of the location.
+    protected static final String [] PACTIONS =     // A hard-coded array of possible
+                                     {"Do Nothing", // actions. 
+                                      "Buy",       
+                                      "Sell", 
+                                      "Improve Property",
+                                      "End Game"};              
   
-    protected String [] possibleActions; 
+    protected String [] possibleActions;            // Array stroing possibleActions
     public boolean [] actionStatus;                 // An array to hold which actions
                                                     //   are enabled and disabled
                                                     //   for a given board location.
@@ -27,7 +29,7 @@ public abstract class boardLocation
     //       the class member nameOfLocation is set to "".
     //       the class member spacesFromGo is set to 0.
     //       the class member possibleActions initialized.
-    //       the calss member actionStatus[i] = false   for 0 < i < actionStatus.Length
+    //       the class member actionStatus is initialized to false.
     {
         this("",0);
     }
@@ -39,20 +41,22 @@ public abstract class boardLocation
     //       the class member spacesFromGo is set to spacesFromGo.
     //       the class member possibleActions initialized.
     {
-        this.nameOfLocation = nameOfLocation;
+        this.nameOfLocation = nameOfLocation;       // Initializing class members.
         this.spacesFromGo = spacesFromGo;        
         
         this.actionStatus = new boolean [5];
         possibleActions = new String [5];
-        for(String pActions : possibleActions)
-        {
+        
+        for(String pActions : possibleActions)      // Initializing possibleActions to
+        {                                           // ""
             pActions = "";
         }
-        // Initializing the aStatus boolean array
-        for(boolean aStatus : actionStatus)
+                                                    // Initializing the aStatus boolean
+        for(boolean aStatus : actionStatus)         // array to false.
+        {
             aStatus = false;
+        }
     }
-    
     
     public void setSpacesFromGo (int numSpaces)
     // PRE: numSpaces must be initialized
@@ -75,27 +79,30 @@ public abstract class boardLocation
         return this.spacesFromGo;
     }
     
-    
-    
     public String getName()
     // POST: FCTVAL == The class member nameOfLocation, it represents
     //                 the name of the location.
     {
         return this.nameOfLocation;
     }
-
-    public abstract boolean performAction(player thePlayer, player theBank, char choice); 
+    
     
     public abstract String [] getPossibleActions(player player);
+    // PRE: player must be initialized.
+    // POST: This is an abstract class that returns a string array
+    //       of all the possible actions a player can perform at a 
+    //       certain state of the game.
     
     public boolean [] getActionStatus()
+    // POST: FCTVAL == actionStatus, An array of booleans that represent
+    //                 possible actions.
     {
-        
         return actionStatus;
     }
+    
     @Override
     public String toString()
-    // POST:  returns a string representing the object of the board location
+    // POST:  returns a string representing an object of the board location
     //        class.
     {
         return "The name of this location is : " + nameOfLocation
